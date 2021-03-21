c8d3afc87b4a597cf0473692773054c57dc93fcc
copolat <copolat@yahoo.com>
2021-03-20 19:46:50 -0600

    Add ROUTE in APP.JS component and link it to MoviesPage component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { Link, Route } from 'react-router-dom';
+import MoviesPage from './components/pages/MoviesPage'
 
 function App() {
   return (
     <div className="App">
       App Component
+      <br/>
+      <Link to="movies">Movies</Link> {/* This link works as same as <a href="">... from HTML */}
+      <Route path="./movies" component={MoviesPage}></Route>
     </div>
   );
 }
