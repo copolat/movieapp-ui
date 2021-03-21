6e835777491a63b7c62c83b576a63b18ba74b40d
copolat <copolat@yahoo.com>
2021-03-21 14:49:44 -0600

    Fix  MovieList module export error

--- src/components/pages/MoviesPage.js
+++ src/components/pages/MoviesPage.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import MovieList from '../pages'
+import MovieList from '../MovieList';
 
 export class MoviesPage extends Component {
 
